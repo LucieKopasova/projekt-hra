df2df0bdf782c945ceb587c4954bf1d5f16e468e
Lucie Kopasová <luca.kopasova@seznam.cz>
2025-05-16 14:53:56 +0200

    založení projektu

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,15 @@
+import './style.css';
+
+export const HomePage = () => {
+  return (
+    <div className="container">
+      <header>
+        <h1>Výprava Batoně</h1>
+      </header>
+      <main>
+        <p></p>
+      </main>
+      <footer></footer>
+    </div>
+  );
+};
