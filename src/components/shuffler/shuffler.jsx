7939982c170a6204798d64ef64777c54f9eac717
Lucie Kopasová <luca.kopasova@seznam.cz>
2025-05-19 11:02:05 +0200

    úuprava proměných v Shuffleru

--- src/components/shuffler/shuffler.jsx
+++ src/components/shuffler/shuffler.jsx
@@ -6,38 +6,38 @@ const blocker = 3;
 
 //nahodný výber příkladu z pole
 
-const randomNumber = Math.floor(Math.random() * 3) + 1;
+const randomPropertyKey = Math.floor(Math.random() * 3) + 1;
 
 //správný výsledek  jako je  překážka
-const correctResult = () => {
+const getCorrectResult = () => {
   const object = examples.find((obj) => obj.id === blocker);
-  return `Spravný výsledek: ${object[randomNumber]}`;
+  return `Spravný výsledek: ${object[randomPropertyKey]}`;
 };
 //menší vysledek než je překážka ale větší než je nula
 
-const smallerResult = () => {
+const getLowerResult = () => {
   const smallerthenBlocker = Math.ceil(Math.random() * (blocker - 1));
   const object = examples.find((obj) => obj.id === smallerthenBlocker);
-  return `Nizká hodnota: ${object[randomNumber]}`;
+  return `Nizká hodnota: ${object[randomPropertyKey]}`;
 };
 
 //větší výsledek než je překážka o 2
 
-const higherResult = () => {
+const getHigherResult = () => {
   const higherthenBlocker =
     Math.floor(Math.random() * (examples.length - blocker)) + blocker + 1;
   const object = examples.find((obj) => obj.id === higherthenBlocker);
-  return `Vysoká hodnota: ${object[randomNumber]}`;
+  return `Vysoká hodnota: ${object[randomPropertyKey]}`;
 };
 
 // vytvoří pole s příklady
-const resultArray = [correctResult(), smallerResult(), higherResult()];
+const resultArray = [getCorrectResult(), getLowerResult(), getHigherResult()];
 
 // zamíchá pole
-const shuffler = (array) => {
+const shuffleArray = (array) => {
   return array.sort(() => Math.random() - 0.5);
 };
-const shuffledResults = shuffler(resultArray);
+const shuffledResults = shuffleArray(resultArray);
 
 export const Exams = ({}) => {
   return (
