77b0a6a5dbd2148f6a959d26e2cb14b28e46c6f5
Lucie Kopasová <luca.kopasova@seznam.cz>
2025-05-22 15:56:46 +0200

    přidání soundtracku

---
+++ src/components/soundtrack.jsx/soundtrack.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import useSound from 'use-sound';
+import soundtrackSong from '../../sounds/dragons.mp3';
+import './soundtrack.css';
+
+export const Soundtrack = () => {
+  const [play, { stop }] = useSound(soundtrackSong);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  /*   useEffect(() => {
+    const handleMusicClick = () => {
+      play();
+      setIsPlaying(true);
+    };
+    document.addEventListener('click', handleMusicClick);
+  }, []);
+ */
+  const toggleSound = () => {
+    if (isPlaying === true) {
+      stop();
+    } else {
+      play();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
+  return (
+    <button className="music--button" onClick={toggleSound}>
+      {isPlaying ? 'Zastavit zvuk' : 'Přehrát zvuk'}{' '}
+    </button>
+  );
+};
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,16 +1,18 @@
 import { Exams } from '../../components/shuffler/shuffler';
+import { Soundtrack } from '../../components/soundtrack.jsx/soundtrack';
 import { TypewriterText } from '../../components/TypewriterText/TypewriterText';
 import './style.css';
 
 export const HomePage = () => {
   return (
     <div className="container">
+      <Soundtrack />
       <header>
         <h1>Výprava Batoně</h1>
       </header>
       <main>
         <Exams />
-        <TypewriterText/>
+        <TypewriterText />
       </main>
       <footer></footer>
     </div>
