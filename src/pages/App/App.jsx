6338601148734b00b106df6433bc4e77b139abf8
Jana Nekvapilová <jana.nekva@gmail.com>
2025-05-26 08:45:45 +0200

    struktura Homepage a základní CSS

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,15 +1,10 @@
 import { Outlet } from 'react-router-dom';
-import { HealthBar } from '../../components/health/health';
-import { Soundtrack } from '../../components/soundtrack.jsx/soundtrack';
-import { TypewriterText } from '../../components/TypewriterText/TypewriterText';
 import './style.css';
 
 export const App = () => {
   return (
     <div className="container">
-     
-        <h1>Výprava Batoně</h1>
-     <Outlet/>
+      <Outlet />
     </div>
   );
 };
