5db9adce278e17d6d2b508d95163bda338fb526e
Jana Nekvapilová <jana.nekva@gmail.com>
2025-06-01 16:34:54 +0200

    rozmístění prvků komponenty GameLevel2

---
+++ src/components/GameLevel/GameLevel2.jsx
@@ -0,0 +1,44 @@
+import './GameLevel2.css';
+import '/global.css';
+import bgImage from '/assets/13_potok.png'; // uprav dle projektu
+
+export const GameLevel2 = () => {
+  return (
+    <div className="page-wrapper"
+ 
+    >
+        <img src="/../assets/12_klada.png" />
+     
+      <div className="game-container">
+        <header className="header">
+          <div className="score">100</div>
+          <div className="header-buttons">
+            <button className="header-button">zvuk</button>
+            <button className="header-button">domů</button>
+          </div>
+        </header>
+
+        
+         
+            <div className="result-exams">3</div>
+            <div className="result-blocker">2</div>
+         
+
+          <div className="examples">
+            <button className="example-button">1 + 1</button>
+            <button className="example-button">2 + 0</button>
+            <button className="example-button">3 - 1</button>
+          </div>
+      
+
+        <section className="blocker"></section>
+
+        <footer className="footer">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, cupiditate!
+        </footer>
+
+      </div>
+      </div>
+   
+  );
+};
--- src/pages/GamePage/GamePage.jsx
+++ src/pages/GamePage/GamePage.jsx
@@ -1,8 +1,9 @@
 import { GameLevel } from '../../components/GameLevel/GameLevel';
 import { GameLevel1 } from '../../components/GameLevel/GameLevel1';
+import { GameLevel2 } from '../../components/GameLevel/GameLevel2';
 
 export const GamePage = () => {
-  let i = 2;
+  let i = 1;
 
-  return <>{i === 1 ? <GameLevel /> : <GameLevel1 />}</>;
+  return <>{i === 1 ? <GameLevel2 /> : <GameLevel1 />}</>;
 };
