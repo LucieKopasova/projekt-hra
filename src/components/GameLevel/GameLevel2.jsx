62fd81487fd133b762063cc490dff1203c6d930d
Lucie Kopasová <luca.kopasova@seznam.cz>
2025-06-03 18:46:16 +0200

    přidání obrazku mapy

--- src/components/GameLevel/GameLevel2.jsx
+++ src/components/GameLevel/GameLevel2.jsx
@@ -1,14 +1,12 @@
 import './GameLevel2.css';
 import '/global.css';
-import bgImage from '/assets/13_potok.png'; // uprav dle projektu
+import bgImage from '/assets/13_liska.png'; // uprav dle projektu
 
 export const GameLevel2 = () => {
   return (
-    <div className="page-wrapper"
- 
-    >
-        <img src="/../assets/12_klada.png" />
-     
+    <div className="page-wrapper">
+      <img src="/../assets/12_klada.png" />
+
       <div className="game-container">
         <header className="header">
           <div className="score">100</div>
@@ -18,30 +16,29 @@ export const GameLevel2 = () => {
           </div>
         </header>
 
-        
-         
-            <div className="result-exams">3</div>
-            <div className="result-blocker">2</div>
-         
+        <div className="result-exams">3</div>
+        <div className="result-blocker">2</div>
 
-          <div className="examples">
-            <button className="example-button">1 + 1</button>
-            <button className="example-button">2 + 0</button>
-            <button className="example-button">3 - 1</button>
-          </div>
-      
+        <div className="examples">
+          <button className="example-button">1 + 1</button>
+          <button className="example-button">2 + 0</button>
+          <button className="example-button">3 - 1</button>
+        </div>
 
         <section className="blocker"></section>
 
         <footer className="footer">
-            <div className='story-wrapper'>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Est sapiente quasi ratione alias cupiditate! Molestiae dolores minima ratione beatae dolorem.</p>
+          <div className="story-wrapper">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem
+              ipsum dolor sit amet consectetur adipisicing elit. Est sapiente
+              quasi ratione alias cupiditate! Molestiae dolores minima ratione
+              beatae dolorem.
+            </p>
             <button className="continue-button">Pokračovat</button>
-            </div>
+          </div>
         </footer>
-
       </div>
-      </div>
-   
+    </div>
   );
 };
--- src/pages/StoryPage/StoryPage.jsx
+++ src/pages/StoryPage/StoryPage.jsx
@@ -1,3 +1,17 @@
+import cedule from '/assets/cedule.png';
+
 export const StoryPage = () => {
-    return <h1>Pribeh</h1>
-}
+  return (
+    <div>
+      <h1>Tohle je Batoň.</h1>
+      <p>
+        Jednoho dne se podíval z okna, zhluboka se nadechl a řekl si: „Dnes je
+        ten den.“ Už dlouho snil o tom, že jednou vyrazí na pořádnou výpravu. A
+        teď je to tady. Batoň se chystá na svou první velkou cestu přes kopce,
+        lesy i louky, daleko od domova. Chce spát pod hvězdami, přebrodit
+        potoky, zdolat opravdový kopec a zažít opravdové dobrodružství. Zabalil
+        si batoh, obul pevné boty… a vyrazil.
+      </p>
+    </div>
+  );
+};
