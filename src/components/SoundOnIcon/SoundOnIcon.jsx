3f0faceefa9c40d8b9fa9f81006fa41907d3079a
Jana Nekvapilová <jana.nekva@gmail.com>
2025-06-04 19:00:39 +0200

    vytvoření ikon soundOn, soundOff, Home

---
+++ src/components/SoundOnIcon/SoundOnIcon.jsx
@@ -0,0 +1,24 @@
+export const SoundOnIcon = () => {
+    return (
+        <div style={{ 
+            border: "3px solid #412f01",
+           background: "linear-gradient(to bottom, #c8ab63, #a17e3e)", 
+            borderRadius: "7px", 
+            backgroundColor: "#aa8e4a", 
+            width: "50px", 
+            height: "50px", 
+            display: "flex",
+            justifyContent: "center"
+         }}> 
+        <svg width="48" height="48" viewBox="0 0 64 64" fill="#412f01" xmlns="http://www.w3.org/2000/svg">
+            <path d="M8 24V40C8 41.1046 8.89543 42 10 42H18L32 54V10L18 22H10C8.89543 22 8 22.8954 8 24Z" />
+            <path d="M40 26C42.6667 28.6667 42.6667 35.3333 40 38" stroke="#3B2600" stroke-width="4" stroke-linecap="round" />
+            <path d="M46 22C49.3333 26 49.3333 38 46 42" stroke="#3B2600" stroke-width="4" stroke-linecap="round" />
+            <path d="M52 18C56 24 56 40 52 46" stroke="#3B2600" stroke-width="4" stroke-linecap="round" />
+        </svg>
+
+        </div>
+
+
+    )
+}
--- src/pages/DevPage/DevPage.jsx
+++ src/pages/DevPage/DevPage.jsx
@@ -1,15 +1,22 @@
 import { HealthBar } from "../../components/health/health";
+import { HomeIcon } from "../../components/HomeIcon/HomeIcon";
+import { SoundOffIcon } from "../../components/SoundOffIcon/SoundOffIcon";
+import { SoundOnIcon } from "../../components/SoundOnIcon/SoundOnIcon";
 import { Soundtrack } from "../../components/soundtrack.jsx/soundtrack";
 import { TypewriterText } from "../../components/TypewriterText/TypewriterText";
 
 export const DevPage = () => {
   return (
-    <div className="container">
+    <div className="container" style={{padding: "20px"}}>
       <Soundtrack />
       <header>
         <h1>Výprava Batoně</h1>
       </header>
       <main>
+        <HomeIcon/>
+        <SoundOffIcon/>
+        <SoundOnIcon/>
+        
         <HealthBar />
         <TypewriterText />
       </main>
