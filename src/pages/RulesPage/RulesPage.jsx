39ff9386d3767bd434b620baac6f73c53a1c7818
Lucie Kopasová <luca.kopasova@seznam.cz>
2025-06-06 14:21:24 +0200

    přidání stránky pravidla

--- src/pages/RulesPage/RulesPage.jsx
+++ src/pages/RulesPage/RulesPage.jsx
@@ -1,7 +1,21 @@
 export const RulesPage = () => {
   return (
-    <div className="homepage-content">
-      <h1>Pravidla</h1>
+    <div className="rulesPage-content">
+      <section className="rulesPage-text-container">
+        <h2 className="rulesPage-title">Pravidla</h2>
+        <p className="rulesPage-text">
+          ​​Batoň se vydal na velkou výpravu! Je plný síly a má spoustu
+          energie,kterou uvidíš v horní části obrazovky. Na každém políčku
+          uvidíš, kolik energie Batoň potřebuje na splnění úkolu (například 5).
+          Tvým úkolem je vybrat takový příklad (například 2 + 3), který dává
+          správné číslo. Když vybereš správně, Batoň může pokračovat dál. Když
+          vybereš menší číslo, musíš to zkusit znovu. Když vybereš větší číslo,
+          Batoň sice pokračuje, ale ztratí víc energie, než by musel. Hodnota z
+          každého vybraného příkladu se odečte od skóre. Vyhraješ, když Batoň
+          dojde až do cíle dřív, než mu dojde energie.
+        </p>
+      </section>
+      <button className="arrow">Pokračovat</button>
     </div>
   );
 };
