5ca2398c47f49e89e9d8c122b39e252dfb801a55
Jana Nekvapilová <jana.nekva@gmail.com>
2025-06-08 12:34:56 +0200

    míchání příkladů jen při prvním načetní komponenty

--- src/components/GameSchuffler/GameSchuffler.jsx
+++ src/components/GameSchuffler/GameSchuffler.jsx
@@ -1,3 +1,5 @@
+
+import { useState } from 'react';
 import examples from '../../data/examples.json';
 import useSound from 'use-sound';
 import soundclik from '../../sounds/mouse-click.mp3';
@@ -9,59 +11,63 @@ export const Gameschuffler = ({
 }) => {
   const [play] = useSound(soundclik);
 
-  // hodnota přkážky
-  const blocker = initBlockerTarget;
-  const currentBlockerResult = blockerTarget;
+  const initResults = () => {
+    // hodnota přkážky
+    const blocker = initBlockerTarget;
 
-  //nahodný výber příkladu z pole
+    //nahodný výber příkladu z pole
+    const randomPropertyKey = Math.floor(Math.random() * 15) + 1;
 
-  const randomPropertyKey = Math.floor(Math.random() * 15) + 1;
-
-  //správný výsledek  jako je  překážka
-  const getCorrectResult = () => {
-    const object = examples.find((obj) => obj.examplesValue === blocker);
-    return {
-      text: object[randomPropertyKey],
-      examplesValue: object.examplesValue,
+    //správný výsledek  jako je  překážka
+    const getCorrectResult = () => {
+      const object = examples.find((obj) => obj.examplesValue === blocker);
+      return {
+        text: object[randomPropertyKey],
+        examplesValue: object.examplesValue,
+      };
     };
-  };
-  //menší vysledek než je překážka ale větší než je nula
+    //menší vysledek než je překážka ale větší než je nula
 
-  const getLowerResult = () => {
-    const smallerthenBlocker = Math.ceil(Math.random() * (blocker - 1));
-    const object = examples.find(
-      (obj) => obj.examplesValue === smallerthenBlocker,
-    );
-    return {
-      text: object[randomPropertyKey],
-      examplesValue: object.examplesValue,
+    const getLowerResult = () => {
+      const smallerthenBlocker = Math.ceil(Math.random() * (blocker - 1));
+      const object = examples.find(
+        (obj) => obj.examplesValue === smallerthenBlocker,
+      );
+      return {
+        text: object[randomPropertyKey],
+        examplesValue: object.examplesValue,
+      };
     };
-  };
 
-  //větší výsledek než je překážka o 2
+    //větší výsledek než je překážka o 2
 
-  const getHigherResult = () => {
-    const higherthenBlocker =
-      Math.floor(Math.random() * (examples.length - blocker)) + blocker + 1;
-    const object = examples.find(
-      (obj) => obj.examplesValue === higherthenBlocker,
-    );
-    return {
-      text: object[randomPropertyKey],
-      examplesValue: object.examplesValue,
+    const getHigherResult = () => {
+      const higherthenBlocker =
+        Math.floor(Math.random() * (examples.length - blocker)) + blocker + 1;
+      const object = examples.find(
+        (obj) => obj.examplesValue === higherthenBlocker,
+      );
+      return {
+        text: object[randomPropertyKey],
+        examplesValue: object.examplesValue,
+      };
     };
-  };
 
-  // vytvoří pole s příklady
-  const resultArray = [getCorrectResult(), getLowerResult(), getHigherResult()];
+    // vytvoří pole s příklady
+    const resultArray = [getCorrectResult(), getLowerResult(), getHigherResult()];
 
-  // zamíchá pole
-  const shuffleArray = (array) => {
-    return array.sort(() => Math.random() - 0.5);
-  };
-  const shuffledResults = shuffleArray(resultArray);
+    // zamíchá pole
+    const shuffleArray = (array) => {
+      return array.sort(() => Math.random() - 0.5);
+    };
 
-  console.log('shuffler' + blocker);
+
+    return shuffleArray(resultArray);
+  }
+
+  const [shuffledResults] = useState(initResults)
+
+
   //RETURN --------------------------
   //---------------------------------
 
@@ -75,7 +81,7 @@ export const Gameschuffler = ({
               play();
               onExams(result.examplesValue);
             }}
-            disabled={currentBlockerResult <= 0}
+            disabled={blockerTarget <= 0}
           >
             {result.text}
           </button>
