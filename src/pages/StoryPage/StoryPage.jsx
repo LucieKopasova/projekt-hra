b88918dbb8a3eb373a471b6b6ee1c78187fc2996
Jana Nekvapilová <jana.nekva@gmail.com>
2025-06-08 22:24:22 +0200

    úprava stylů layoutu StoryPage

--- src/pages/StoryPage/StoryPage.jsx
+++ src/pages/StoryPage/StoryPage.jsx
@@ -10,7 +10,8 @@ export const StoryPage = () => {
           <p>
             Jednoho dne se podíval z okna, zhluboka se nadechl a řekl si: „Dnes
             je ten den.“ Už dlouho snil o tom, že jednou vyrazí na pořádnou
-            výpravu. A teď je to tady. Batoň se chystá na svou první velkou
+            výpravu. A teď je to tady. </p>
+            <p>  Batoň se chystá na svou první velkou
             cestu přes kopce, lesy i louky, daleko od domova. Chce spát pod
             hvězdami, přebrodit potoky, zdolat opravdový kopec a zažít opravdové
             dobrodružství. Zabalil si batoh, obul pevné boty… a vyrazil.
